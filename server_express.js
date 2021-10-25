e5acc18b5e741793c2dfdbed6f54305faccab1fa
SeoyeonKim93 <hiyeeonbly@gmail.com>
2021-10-25 20:24:01 +0900

    노드 서버

--- server_express.js
+++ server_express.js
@@ -2,10 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
+const multer = require("multer");
+// 아래 함수처럼 써줘야 image의 이름 그대로 uploads 파일에 post할 수 있음
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 const port = 8080;
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
 // products경로로 GET 명령을 했을 때 두번째 인자 함수가 실행 됨
 app.get("/products", (req, res) => {
@@ -17,6 +30,7 @@ app.get("/products", (req, res) => {
     attributes: ["id", "name", "price", "createdAt", "seller", "imageurl"],
   })
     .then((result) => {
+      console.log("PRODUCTS : ", result);
       res.send({
         products: result,
       });
@@ -24,6 +38,7 @@ app.get("/products", (req, res) => {
     .catch((error) => {
       console.error(error);
       res.send("에러발생");
+      res.status(400).send("에러발생");
       //   const query = req.query;
       //   console.log("QUERY:", query);
       //   res.send({
@@ -57,43 +72,45 @@ app.get("/products", (req, res) => {
 // post method도 이용해보기 위해서는 postman 사용하면 됨!
 app.post("/products", (req, res) => {
   // 2. 쌓여있는 Data 조회해서 결과 보여주기
-  models.Product.findAll()
-    .then((result) => {
-      res.send({
-        products: result,
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.send("에러발생");
-    });
-  // 1. 직접 DB에 데이터 입력해서 결과 보여주기
-  // // POST는 바디에 data 담을 수 있음
-  // const body = req.body;
-  // const { name, description, price, seller } = body;
-  // // 방어 코드 생성
-  // if (!name || !description || !price || !seller) {
-  //   res.send("모든 필드를 입력해주세요");
-  // }
-  // models.Product.create({
-  //   name,
-  //   description,
-  //   price,
-  //   seller,
-  // })
+  // models.Product.findAll()
   //   .then((result) => {
-  //     console.log("상품 생성 결과: ", result);
   //     res.send({
-  //       result,
+  //       products: result,
   //     });
   //   })
   //   .catch((error) => {
   //     console.error(error);
-  //     res.send("상품 업로드에 문제가 발생했습니다.");
+  //     res.send("에러발생");
   //   });
-  // // res.send({
-  // //   body: body,
-  // // });
+
+  // 1. 직접 DB에 데이터 입력해서 결과 보여주기
+  // // POST는 바디에 data 담을 수 있음
+  const body = req.body;
+  const { name, description, price, seller, imageurl } = body;
+  // // 방어 코드 생성
+  if (!name || !description || !price || !seller || !imageurl) {
+    res.status(400).send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+    imageurl,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과: ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 업로드에 문제가 발생했습니다.");
+    });
+  // res.send({
+  //   body: body,
+  // });
 });
 
 app.get("/products/:id", (req, res) => {
@@ -111,9 +128,21 @@ app.get("/products/:id", (req, res) => {
       });
     })
     .catch((error) => {
-      console.log("상품 조회에 에러가 발생했습니다.");
+      res.status(400).send("상품 조회에 에러가 발생했습니다");
     });
   // res.send(`id는 ${id}입니다.`);
+});
+
+// multer : 파일 처리 라이브러리
+// single : 이미지 파일을 하나만 보냈을 때 처리하는 구문
+// 1. multer - postman을 통해서 image POST 해보기
+app.post("/image", upload.single("image"), (req, res) => {
+  // 저장된 이미지 파일의 정보를 받을 수 있음
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageurl: file.path,
+  });
 });
 
 app.listen(port, () => {
